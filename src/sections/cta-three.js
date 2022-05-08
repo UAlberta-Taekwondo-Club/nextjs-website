659ea7c5d3991a9823fa8356d919ecbc9788ddf6
weichen <w3ichen@gmail.com>
2022-05-08 13:10:30 -0600

    Positioning of grid cards is done

--- src/sections/cta-three.js
+++ src/sections/cta-three.js
@@ -1,46 +1,51 @@
 import React from 'react';
-import { Box, Container, Flex, Grid, Heading, Text } from 'theme-ui';
+import { Box, Grid, Heading } from 'theme-ui';
 
 const CtaThree = () => {
   return (
     <Box as="section" sx={styles.ctaThree}>
-      <Container>
-        <Flex sx={styles.ctaThree.row}>
-
-          <Box sx={styles.ctaThree.colOne}>
-            <Grid sx={styles.ctaThree.grid}>
-              <Box sx={styles.ctaThree.box}>
-                <Heading as="h3" sx={styles.ctaThree.color1}>
-                  80K+
-                </Heading>
-                <Text as="p">{'Student-led ~ Student delivered ~ Student-focused'}</Text>
-              </Box>
-              <Box sx={styles.ctaThree.box}>
-                <Heading sx={styles.ctaThree.color2} as="h3">
-                  150+
-                </Heading>
-                <Text as="p">
-                  {'Beginners welcome'}
-                </Text>
-              </Box>
-              <Box sx={styles.ctaThree.box}>
-                <Heading sx={styles.ctaThree.color3} as="h3">
-                  image here
-                </Heading>
-                <Text as="p">{'Daily updated blog \n post maintain'}</Text>
-              </Box>
-              <Box sx={styles.ctaThree.box}>
-                <Heading sx={styles.ctaThree.color4} as="h3">
-                  & 3M
-                </Heading>
-                <Text as="p">
-                  {'Job posted everydays \n with  qualification'}
-                </Text>
-              </Box>
-            </Grid>
-          </Box>
-        </Flex>
-      </Container>
+      <Grid sx={styles.ctaThree.grid}>
+        <Box sx={styles.ctaThree.box}>
+          <Heading sx={{ color: '#FF753A' }} as="h3">
+            Beginners welcome
+          </Heading>
+        </Box>
+        <Box sx={styles.ctaThree.box}>
+          <Heading as="h3" sx={{ color: '#EF9E48' }}>
+            Student led<br />Student delivered<br />Student focused
+          </Heading>
+        </Box>
+        <Box sx={styles.ctaThree.box}>
+          <Heading sx={{ color: '#FA578E' }} as="h3">
+            Social events
+          </Heading>
+        </Box>
+        <Box sx={styles.ctaThree.box}>
+          <Heading sx={{ color: '#E682FF' }} as="h3">
+            Lowest fees
+          </Heading>
+        </Box>
+        <Box sx={styles.ctaThree.box}>
+          <Heading sx={{ color: '#EF9E48' }} as="h3">
+            Kicks
+          </Heading>
+        </Box>
+        <Box sx={styles.ctaThree.box}>
+          <Heading sx={{ color: '#FA578E' }} as="h3">
+            Sparring
+          </Heading>
+        </Box>
+        <Box sx={styles.ctaThree.box}>
+          <Heading sx={{ color: '#E682FF' }} as="h3">
+            Strength
+          </Heading>
+        </Box>
+        <Box sx={styles.ctaThree.box}>
+          <Heading sx={{ color: '#FF753A' }} as="h3">
+            Flexibility
+          </Heading>
+        </Box>
+      </Grid>
     </Box>
   );
 };
@@ -49,25 +54,15 @@ export default CtaThree;
 
 const styles = {
   ctaThree: {
-    paddingTop: [0, null, null, null, 50, 90],
-    paddingBottom: [120, null, null, null, 100, 220],
-    row: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: ['column', null, null, null, 'row-reverse'],
-    },
-    colOne: {
-      flex: ['0 0 100%', null, null, null, '0 0 55%'],
-      paddingRight: ['0', null, null, null, null, '34px'],
-    },
+    pt: 100,
+    pb: 200, // Need to account for the `top: 50px`
+    px: 25,
+    display: "flex",
     grid: {
       display: 'grid',
       gridGap: '45px',
-      gridTemplateColumns: '1fr 1fr',
-      '@media(max-width:1199px)': {
-        gridGap: '30px',
-      },
-      '@media(max-width:425px)': {
+      gridTemplateColumns: '1fr 1fr 1fr 1fr',
+      '@media(max-width:1000px)': {
         gridTemplateColumns: '1fr 1fr',
         gridGap: '20px',
       },
@@ -77,71 +72,18 @@ const styles = {
       borderRadius: '10px',
       textAlign: 'center',
       position: 'relative',
-      paddingTop: '95.5px',
-      paddingBottom: '95.5px',
-      '&:nth-of-type(1)': {
-        top: '70px',
-      },
-      '&:nth-of-type(3)': {
-        top: '70px',
-      },
-      '@media(max-width:1199px)': {
-        paddingTop: '35.5px',
-        paddingBottom: '35.5px',
-        '&:nth-of-type(1)': {
-          top: '40px',
-        },
-        '&:nth-of-type(3)': {
-          top: '40px',
-        },
-      },
-      '@media(max-width:425px)': {
-        '&:nth-of-type(1)': {
-          top: '0px',
-        },
-        '&:nth-of-type(3)': {
-          top: '0px',
-        },
+      py: "50px",
+      px: "30px",
+      '&:nth-of-type(odd)': {
+        top: '50px',
       },
       h3: {
         margin: 0,
-        fontSize: '36px',
+        fontSize: '30px',
         lineHeight: 1,
         fontWeight: 700,
         letterSpacing: '-1.5px',
-        '@media(min-width:1281px)': {
-          fontSize: '55px',
-        },
-        '@media(min-width:1441px)': {
-          fontSize: '72px',
-        },
       },
-      '> p': {
-        margin: 0,
-        marginTop: '7px',
-        fontSize: '18px',
-        color: '#0F2137',
-        lineHeight: 1.39,
-        letterSpacing: '-0.5px',
-        opacity: 0.7,
-        whiteSpace: 'pre-line',
-        '@media(max-width:575px)': {
-          fontSize: '15px',
-          lineHeight: 1.65,
-        },
-      },
-    },
-    color1: {
-      color: '#EF9E48',
-    },
-    color2: {
-      color: '#FF753A',
-    },
-    color3: {
-      color: '#FA578E',
-    },
-    color4: {
-      color: '#E682FF',
     },
   },
 };
